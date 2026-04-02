815ad97b3a51cc4f09ad6179a4fe8f6911565efa
RajuTechAssist <oooye.raju@gmail.com>
2026-04-02 09:42:12 +0530

    fixed image reloading slow by instaling image-optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -9,7 +10,16 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        ViteImageOptimizer({
+          png: { quality: 90 },
+          jpeg: { quality: 90 },
+          jpg: { quality: 90 },
+          webp: { quality: 90 },
+          avif: { quality: 90 },
+        }),
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
