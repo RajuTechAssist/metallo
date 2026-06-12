bfb40aa0d73af464cf3c2592096cb3ef35007b01
RajuTechAssist <oooye.raju@gmail.com>
2026-06-12 12:41:52 +0530

    updated and changed fabricated structure images

--- config/images/fabricated.ts
+++ config/images/fabricated.ts
@@ -3,13 +3,13 @@
 export const FABRICATED_STRUCTURES_IMAGES = {
   hero: '/fabricated-structures/fabricated_structure4.png',
   products: {
-    preEngineeredBuilding: '/fabricated-structures/pre-engineered-building.png',
-    primaryFraming: '/fabricated-structures/primary-framing.png',
-    secondaryFraming: '/fabricated-structures/secondary-framing.png',
-    cranesMezzanines: '/fabricated-structures/cranes-mezzanines.png',
-    coldFormSection: '/fabricated-structures/cold-form-section.png',
-    metalRoofingCladding: '/fabricated-structures/metal-roofing-cladding.png',
-    decking: '/fabricated-structures/decking.png',
+    preEngineeredBuilding: '/fabricated-structures/pre-engineered-building1.png',
+    primaryFraming: '/fabricated-structures/primary-framing1.png',
+    secondaryFraming: '/fabricated-structures/secondary-framing1.png',
+    cranesMezzanines: '/fabricated-structures/cranes-mezzanines1.png',
+    coldFormSection: '/fabricated-structures/cold-form-section1.png',
+    metalRoofingCladding: '/fabricated-structures/metal-roofing-cladding1.png',
+    decking: '/fabricated-structures/decking1.png',
     specialisedStructuralFabrication: '/fabricated-structures/specialised-structural-fabrication.png',
   }
 } as const;
